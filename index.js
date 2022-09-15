7a0ec49b74d00c408c634a02f463f17ac9ace853
Artjom Strelkov <strelkov.artjom@mail.ru>
2022-09-15 13:08:18 +0300

    fixing error v2

--- index.js
+++ index.js
@@ -60,4 +60,4 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root
 }))
 
-app.listen(5000, () => console.log("Server started on port 5000".yellow))
+app.listen(process.env.PORT || 3000, () => console.log("Server started on port 3000".yellow))
